c76f94001f3726e85a917c5e9da42d56ac445d70
Chau Nguyen <chaunguyen4392@gmail.com>
2022-06-04 15:00:42 +0700

    Add transaction explorer link to SwapRefunded Page

--- src/containers/SwapRefunded/SwapRefunded.js
+++ src/containers/SwapRefunded/SwapRefunded.js
@@ -4,43 +4,77 @@ import HexaDisplay from '../../components/HexaDisplay/HexaDisplay'
 import Button from '../../components/Button/Button'
 import { assets as cryptoassets } from '@liquality/cryptoassets'
 import { APP_BASE_URL } from '../../utils/app-links'
+import { shortenTransactionHash, getExplorerLink } from '../../utils/transactions'
 
 import './SwapRefunded.css'
 
 class SwapRefunded extends Component {
   render () {
     const title = cryptoassets[this.props.assets.a.currency].code + ' SUCCESFULLY RECLAIMED'
-    return <div>
-      <BrandCard className='SwapRefunded' title={title}>
-        <div className='SwapRefunded_expiredFrame'>
-          <div className='SwapRefunded_expiredFrame_content'>
-
-            <p className='SwapRefunded_expiredFrame_content_title'>
-              Expired Swap offer
-            </p>
-            <hr />
-            <p>
-              Receive: <span className='SwapRefunded_expiredFrame_content_value'>{this.props.assets.b.value.toFixed()} {cryptoassets[this.props.assets.b.currency].code}</span>
-            </p>
-            <p>
-              For: <span className='SwapRefunded_expiredFrame_content_value'>{this.props.assets.a.value.toFixed()} {cryptoassets[this.props.assets.a.currency].code}</span>
-            </p>
-            <p>
-              Expired: <span className='SwapRefunded_expiredFrame_content_value'>{this.props.expiration.format('DD/MM/YYYY h:mm a')}</span>
-            </p>
-            <hr />
+    return (
+      <div>
+        <BrandCard className="SwapRefunded" title={title}>
+          <div className="SwapRefunded_expiredFrame">
+            <div className="SwapRefunded_expiredFrame_content">
+              <p className="SwapRefunded_expiredFrame_content_title">
+                Expired Swap offer
+              </p>
+              <hr />
+              <p>
+                Receive:{" "}
+                <span className="SwapRefunded_expiredFrame_content_value">
+                  {this.props.assets.b.value.toFixed()}{" "}
+                  {cryptoassets[this.props.assets.b.currency].code}
+                </span>
+              </p>
+              <p>
+                For:{" "}
+                <span className="SwapRefunded_expiredFrame_content_value">
+                  {this.props.assets.a.value.toFixed()}{" "}
+                  {cryptoassets[this.props.assets.a.currency].code}
+                </span>
+              </p>
+              <p>
+                Expired:{" "}
+                <span className="SwapRefunded_expiredFrame_content_value">
+                  {this.props.expiration.format("DD/MM/YYYY h:mm a")}
+                </span>
+              </p>
+              <hr />
+            </div>
           </div>
-        </div>
-        <div className='SwapRefunded_confirmation'>
-          <p className='SwapRefunded_terms'>
-            {this.props.assets.a.value.toFixed()} {cryptoassets[this.props.assets.a.currency].code} Returned
-          </p>
-          <p>Address: <HexaDisplay address={this.props.wallets.a.addresses[0]} /></p>
-          <p>Transaction: <HexaDisplay address={this.props.refundTransaction.hash} /></p>
-        </div>
-        <Button className='SwapRefunded_button' wide primary onClick={() => window.location.replace(APP_BASE_URL)}>Start another Swap</Button>
-      </BrandCard>
-    </div>
+          <div className="SwapRefunded_confirmation">
+            <p className="SwapRefunded_terms">
+              {this.props.assets.a.value.toFixed()}{" "}
+              {cryptoassets[this.props.assets.a.currency].code} Returned
+            </p>
+            <p>
+              Address:{" "}
+              <HexaDisplay address={this.props.wallets.a.addresses[0]} />
+            </p>
+            <p>
+              Transaction:{" "}
+              <a
+                  className="SwapRefunded_transactionHash"
+                  href={getExplorerLink(this.props.refundTransaction, this.props.assets.a.currency)}
+                  target="_blank"
+                  rel="noopener noreferrer"
+              >
+                {shortenTransactionHash(this.props.refundTransaction.hash)}
+              </a>
+            </p>
+          </div>
+          <Button
+            className="SwapRefunded_button"
+            wide
+            primary
+            onClick={() => window.location.replace(APP_BASE_URL)}
+          >
+            Start another Swap
+          </Button>
+        </BrandCard>
+      </div>
+    );
   }
 }
 
